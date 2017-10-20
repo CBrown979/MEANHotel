c091cf941e547d696ad1fd4f40f92ce62bcdca4a
CBrown979 <candicebrown97@gmail.com>
2017-10-19 23:48:40 -0400

    completed vid 8: Express Routing

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var app = express(); //initial express to create the application
+var path = require('path');
 
 app.set('port', 3000);
+
+app.get('/', function(req, res){
+  console.log("GET the homepage");
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));//passing in filenames as strings avoids potential syntax issues w/ other directory structures on other operating systems
+});
+//the function in the route takes 2 parameters:
+//request (req) object: contains data about the incoming request
+//response (res) object: contains data & methods relating to a response
+
+app.get('/json', function(req, res){
+    console.log("GET the json");
+    res
+      .status(200)
+      .json({"jsonData" : true});
+  });
+
+app.get('/file', function(req, res){
+    console.log("GET the file");
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, 'app.js')); //.join allows multiple parameters to create a file path
+});
 
 var server = app.listen(app.get('port'), function(){
  var port = server.address().port;
  console.log('Magic happens on port ' + port);    
 });
+
 //console.log('Me first!'); //validates that app.listen is asynchronous
 
 //recap of above - we've learned how to:
@@ -16,6 +42,20 @@ var server = app.listen(app.get('port'), function(){
 // How to listen for requests on that particular port
 
 // Also learned that app.listen is asynchronous
+
+//The idea of routing: 
+//Listening for requests on specific URLs
+//Doing something on the server
+//Sending a response
+
+
+
+
+
+
+
+
+
 
 
 
